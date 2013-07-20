8fb58664edaf130fc08e7373cb2a8541765f380e
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-07-20 15:28:45 +0300

    Create grid

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ var analyze = function( dom ) {
 		_analyze( $(this) );
 	});
 }
+
+$().ready( function() {
+	alert("Kissat <3");
+
+	// '<div style="width:100px;height:100px;background:blue;">Kissa</div>'
+	var container = $('<div>', { id: 'container'});
+
+	for(var i=0;i<9;i++){
+        var d = $('<div>', { class: 'box'}) ;
+	    container.append( d );        
+    }
+
+    $('body').prepend( container );
+
+})
