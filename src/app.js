f0bc68e887ea48c1602537df39c47f5fb76a7f81
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-07-20 16:05:14 +0300

    Removed dead code

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-var _analyze = function( text ) {
-	var t = _.escape( text.html() );
-	var l = t.length;
-	text.append("<span style='color:blue;background:green'>" + l + "</span>");
-}
-
-var analyze = function( dom ) {
-	$.each( dom, function() {
-		_analyze( $(this) );
-	});
-}
-
 $().ready( function() {
 	alert("Kissat <3");
 
