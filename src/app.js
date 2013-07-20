50beea1024edb30dd8b3d2ab59aa24c3ca7ba193
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-07-20 16:01:03 +0300

    Searching for words

--- src/app.js
+++ src/app.js
@@ -18,10 +18,17 @@ $().ready( function() {
 	var words = ['innovaatio','synenergia','multimedia','kissa','sosiaalinen media', 'talvivaara', 'ammattiliitto', 'pääministeri', 'huippu'];
 	words = _.shuffle( words );
 
-	var container = $('<div>', { id: 'container'});
+	var text = $('body').text();
+
+
+	var container = $('<div>', { id: 'container'} );
 
 	for(var i=0;i<9;i++){
-        var d = $('<div>', { class: 'box', html : words[i] } ) ;
+		var d = $('<div>', { class: 'box', html : words[i] } ) ;
+
+		if( text.indexOf(words[i]) >0 ){
+			d.addClass('found');
+		}
 	    container.append( d );        
     }
 
